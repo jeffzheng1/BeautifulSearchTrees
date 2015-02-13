e3ed03c410dcf73d46e91b3c0ccfef70c1c10b33
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-02-12 20:37:15 -0800

    fix code formatting

--- js/d3Trees.js
+++ js/d3Trees.js
@@ -15,9 +15,9 @@ var runAgain = false;
 
 //Create SVG element
 var mainSvg = d3.select("#canvas")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
 
 var compareSvg = d3.select("#previous")
       .append("svg")
@@ -89,38 +89,38 @@ function reassignChildren(nodeID, tree) {
 
 //appends lines for parent and child
 var drawTreeLines = function (tree, svg) {
-	for (var i = 0; i < tree.length; i++) {
-		var x1 = tree[i]["x_coor"];
-		var y1 = tree[i]["y_coor"];
-		var a = [x1, y1];
-		for (var j = 0; j < tree[i].children.length; j++) {
-			var child = tree[tree[i].children[j]];
-			if (child) {
-				var x2 = child["x_coor"];
-				var y2 = child["y_coor"];
-				var b = [x2, y2];
-      	var c = [x2, y2, 25];
-        	var intersect1 = getIntersections(a, b, c);
-        	x2prime = intersect1.points.intersection1.coords[0];
-        	y2prime = intersect1.points.intersection1.coords[1];
-        	c = [x1, y1, 25];
-        	var intersect2 = getIntersections(a, b, c);
-        	x1prime = intersect2.points.intersection2.coords[0];
-        	y1prime = intersect2.points.intersection2.coords[1];
-        	svg.append("line")
-          	.style("opacity", 0)
-          	.attr("x1", x1prime)
-          	.attr("y1", y1prime)
-          	.attr("x2", x2prime)
-          	.attr("y2", y2prime)
-          	.attr("stroke-width", .75)
-          	.attr("stroke", "black")
-          	.transition().duration(500)
-          	.style("opacity", 1);
+  for (var i = 0; i < tree.length; i++) {
+    var x1 = tree[i]["x_coor"];
+    var y1 = tree[i]["y_coor"];
+    var a = [x1, y1];
+    for (var j = 0; j < tree[i].children.length; j++) {
+      var child = tree[tree[i].children[j]];
+      if (child) {
+        var x2 = child["x_coor"];
+        var y2 = child["y_coor"];
+        var b = [x2, y2];
+        var c = [x2, y2, 25];
+          var intersect1 = getIntersections(a, b, c);
+          x2prime = intersect1.points.intersection1.coords[0];
+          y2prime = intersect1.points.intersection1.coords[1];
+          c = [x1, y1, 25];
+          var intersect2 = getIntersections(a, b, c);
+          x1prime = intersect2.points.intersection2.coords[0];
+          y1prime = intersect2.points.intersection2.coords[1];
+          svg.append("line")
+            .style("opacity", 0)
+            .attr("x1", x1prime)
+            .attr("y1", y1prime)
+            .attr("x2", x2prime)
+            .attr("y2", y2prime)
+            .attr("stroke-width", .75)
+            .attr("stroke", "black")
+            .transition().duration(500)
+            .style("opacity", 1);
       }
-		}
-	}
-};
+    }
+  }
+}
 
  function findNode(nodeID, tree) { 
   for (var i = 0; i < tree.length; i++) {
@@ -179,25 +179,7 @@ function assignCoordinates(tree, svg) {
       }  
     }
   }
-    // for (var i = 0; i < tree.length; i++) {
-    //   for (var j = 0; j < tree.length; j++) {
-    //     if (i != j) { 
-    //       if (tree[i].x_coor == tree[j].x_coor && tree[i].y_coor == tree[j].y_coor && tree[i].x_coor) { 
-    //         if (tree[i].nodeID < tree[j].nodeID) { 
-    //           tree[i].x_coor -= 19.5
-    //           tree[j].x_coor += 10.5
-    //         }
-    //       }
-    //       if ((tree[i].x_coor - tree[j].x_coor) > 0 && (tree[i].x_coor - tree[j].x_coor) < 20 && tree[i].y_coor == tree[j].y_coor && tree[i].x_coor) { 
-    //         if (tree[i].nodeID < tree[j].nodeID) { 
-    //           tree[i].x_coor -= 22.5
-    //           tree[j].x_coor += 15.5
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
-  }
+}
 
 var createVisual = function (svg, tree) {
   resetCoodinates(tree);
@@ -205,9 +187,9 @@ var createVisual = function (svg, tree) {
   while (runAgain) {
     assignCoordinates(tree, svg);
   }
-	$("#canvas").scrollLeft(w*.15);
+  $("#canvas").scrollLeft(w*.15);
   $("#previous").scrollLeft(w*.15);
-	svg.selectAll("line").remove(); //remove old parent-child lines
+  svg.selectAll("line").remove(); //remove old parent-child lines
 
   var reAssign = []
   nodes = d3.range(tree.length).map(function() { return {radius: radius}; })
@@ -239,47 +221,47 @@ var createVisual = function (svg, tree) {
     reassignChildren(reAssign[nodeID], tree);
   }
 
-	//ANIMATES EACH NODE IN TREE
-	//each circle represents a node in the tree
-	color = d3.scale.category10();
-	var circles = svg.selectAll("circle").data(tree, function(d) { return d.nodeID; } ); //binds data to circle element in svg canvas
-	circles.transition().delay(500).duration(500).style("opacity", 1)
-   			.attr("cx", function(d) { //animates existing circles
-    			return d.x_coor;
-    		})
-    		.attr("cy", function(d) {
-    			return d.y_coor;
-    		});
-	circles.enter().append("circle").style("opacity", 0); //adds circles that have binded data to canvas
-	circles.transition().duration(500).style("opacity", .75) //animates and instantiates attributes for new circles
-  			.style("fill", function(d, i) { 
-  				if (d.searched == 1) { 
-  					return "purple";
-  				} 
-  				return color(i % 5)
-  			})	
-  			.attr("cx", function(d) {
-     			return d.x_coor;
-     		})
-     		.attr("cy", function(d) {
-     			return d.y_coor;
-     		})
-     		.attr("r", 25);
-	circles.exit().transition().duration(500).style("opacity", 0).remove(); //removes circles with no data binded
+  //ANIMATES EACH NODE IN TREE
+  //each circle represents a node in the tree
+  color = d3.scale.category10();
+  var circles = svg.selectAll("circle").data(tree, function(d) { return d.nodeID; } ); //binds data to circle element in svg canvas
+  circles.transition().delay(500).duration(500).style("opacity", 1)
+        .attr("cx", function(d) { //animates existing circles
+          return d.x_coor;
+        })
+        .attr("cy", function(d) {
+          return d.y_coor;
+        });
+  circles.enter().append("circle").style("opacity", 0); //adds circles that have binded data to canvas
+  circles.transition().duration(500).style("opacity", .75) //animates and instantiates attributes for new circles
+        .style("fill", function(d, i) { 
+          if (d.searched == 1) { 
+            return "purple";
+          } 
+          return color(i % 5)
+        })  
+        .attr("cx", function(d) {
+          return d.x_coor;
+        })
+        .attr("cy", function(d) {
+          return d.y_coor;
+        })
+        .attr("r", 25);
+  circles.exit().transition().duration(500).style("opacity", 0).remove(); //removes circles with no data binded
 
-	//ANIMATES KEY:VALUE TEXT OF EACH CIRCLE
-	//each label represents key:value text
-	var labels = svg.selectAll(".labels").data(tree, function(d) { return d.nodeID; } ); //binds data to element with class labels in svg canvas
-	labels.transition().delay(500).duration(500).style("opacity", 1)
-	 		  .attr("x", function(d) { //animates existing labels
-	    		return d.x_coor;
-    		})
-    		.attr("y", function(d) {
-    			return d.y_coor;
-    		});
-	labels.enter().append("text").classed("labels", true).style("opacity", 0); //adds text elements that have binded data to canvas
-	labels.text(function(d) { //animates and instantiates attributes for new labels
-   			if (typeof d.key != "object") {
+  //ANIMATES KEY:VALUE TEXT OF EACH CIRCLE
+  //each label represents key:value text
+  var labels = svg.selectAll(".labels").data(tree, function(d) { return d.nodeID; } ); //binds data to element with class labels in svg canvas
+  labels.transition().delay(500).duration(500).style("opacity", 1)
+        .attr("x", function(d) { //animates existing labels
+          return d.x_coor;
+        })
+        .attr("y", function(d) {
+          return d.y_coor;
+        });
+  labels.enter().append("text").classed("labels", true).style("opacity", 0); //adds text elements that have binded data to canvas
+  labels.text(function(d) { //animates and instantiates attributes for new labels
+        if (typeof d.key != "object") {
           return d.key;
         } else { 
           var str = "";
@@ -291,69 +273,69 @@ var createVisual = function (svg, tree) {
           }
           return str;
         }
-   		})
-			.transition().duration(500).style("opacity", 1)
-   		.attr("x", function(d) {
+      })
+      .transition().duration(500).style("opacity", 1)
+      .attr("x", function(d) {
         if (typeof d.key != "object") {
-	   			var str = d.key + "";
-	   			var length = str.length;
-	   			return d.x_coor - length * 3.5;
+          var str = d.key + "";
+          var length = str.length;
+          return d.x_coor - length * 3.5;
         } else {
           var str = ""; 
           for (counter = 0; counter < d.key.length; counter++) { 
-              str += d.key[counter] + "";
-              if (counter < d.key.length-1) { 
-                str += "    ";
-              }
+            str += d.key[counter] + "";
+            if (counter < d.key.length-1) { 
+              str += "    ";
+            }
           }
           var length = str.length/2;
           return d.x_coor - length * 3.5;
         }
-   		})
-   		.attr("y", function(d) {
-   			return d.y_coor + 3.5;
-   		})
-   		.attr("font-weight", "bold")
-   		.attr("font-family", "sans-serif")
-   		.attr("font-size", "12px")
-   		.attr("fill", "black");
-	labels.exit().transition().duration(500).style("opacity", 0).remove(); //removes labels with no data binded
-	drawTreeLines(tree, svg);
+      })
+      .attr("y", function(d) {
+        return d.y_coor + 3.5;
+      })
+      .attr("font-weight", "bold")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "12px")
+      .attr("fill", "black");
+  labels.exit().transition().duration(500).style("opacity", 0).remove(); //removes labels with no data binded
+  drawTreeLines(tree, svg);
   $('#history').attr("data-content", insertHistory);
 };
 
 function addNodeHelper(addKey, tree, svg, history) { 
   if (addKey.indexOf(",") > -1) {
         addManyNodes(addKey+",", tree, history);
-    } else {
-        var numVal = parseInt(addKey);
-        if (numVal != NaN) { 
-            addKey = numVal;
-        }
-        if (len == -1) {
-            createBST(addKey, tree, history);
-        } else {
-            addNode(addKey, tree, history);
-        }
+  } else {
+    var numVal = parseInt(addKey);
+    if (numVal != NaN) { 
+      addKey = numVal;
     }
-    createVisual(svg, tree);
+    if (len == -1) {
+      createBST(addKey, tree, history);
+    } else {
+      addNode(addKey, tree, history);
+    }
+  }
+  createVisual(svg, tree);
 }
 
 addButton.onclick = function(e) {
-    e.preventDefault();
-    var addKey = document.getElementById("addKey").value;
-    addNodeHelper(addKey, currentTree, mainSvg, 1);
-    var tempLen = len;
-    len = -1;
-    previousTree = [];
-    addNodeHelper(insertHistory.slice(0, insertHistory.length-1).toString(), previousTree, compareSvg, 0);
-    len = tempLen;
+  e.preventDefault();
+  var addKey = document.getElementById("addKey").value;
+  addNodeHelper(addKey, currentTree, mainSvg, 1);
+  var tempLen = len;
+  len = -1;
+  previousTree = [];
+  addNodeHelper(insertHistory.slice(0, insertHistory.length-1).toString(), previousTree, compareSvg, 0);
+  len = tempLen;
 };
 
 function removeNodeHelper(deleteKey, tree, svg) { 
   var numVal = parseInt(deleteKey);
   if (numVal != NaN) { 
-      deleteKey = numVal;
+    deleteKey = numVal;
   }
   removeNode(deleteKey, tree);
   createVisual(svg, tree);
@@ -364,7 +346,7 @@ deleteButton.onclick = function(e) {
   var deleteKey = document.getElementById("deleteKey").value;
   var numVal = parseInt(deleteKey);
   if (numVal != NaN) { 
-      deleteKey = numVal;
+    deleteKey = numVal;
   }
   removeNode(deleteKey, currentTree);
   createVisual(mainSvg, currentTree);
